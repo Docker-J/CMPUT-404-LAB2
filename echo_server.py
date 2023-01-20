c822b42cbea845ba61b7f2054557954e2f667c8e
Junesung Lee <junesoung412@gmail.com>
2023-01-19 18:39:54 -0700

    echo_server.py

---
+++ echo_server.py
@@ -0,0 +1,19 @@
+import socket
+
+host = ""
+port = 8001
+buffer_size = 1024
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.bind((host, port))
+s.listen(2)
+
+while True:
+    conn, addr = s.accept()
+    
+    full_data = conn.recv(buffer_size)
+    conn.sendall(full_data)
+    conn.close()
+
+s.close()
